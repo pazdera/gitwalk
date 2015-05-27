361d78a9d8f388b747b8c998090f38b25253bd5e
Radek Pazdera <me@radek.io>
2015-05-27 11:53:15 +0100

    Adding coffeelint to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
 var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffeelint = require('gulp-coffeelint');
+
+var srcDir = './src/',
+    libDir = './lib/';
+
+gulp.task('lint', function () {
+    gulp.src(srcDir + '*.coffee')
+        .pipe(coffeelint())
+        .pipe(coffeelint.reporter());
+});
 
 gulp.task('coffee', function() {
-    gulp.src('./src/*.coffee')
+    gulp.src(srcDir + '.coffee')
         .pipe(coffee({bare: true}).on('error', gutil.log))
-        .pipe(gulp.dest('./lib/'));
+        .pipe(gulp.dest(libDir));
 });
 
 gulp.task('test', ['coffee'], function() {
     console.log('No tests implemented yet.');
 });
 
-gulp.task('default', ['coffee'], function() {
+gulp.task('default', ['lint', 'coffee'], function() {
 });
--- src/gitwalk.coffee
+++ src/gitwalk.coffee
@@ -1,4 +1,4 @@
 # The main of this module
 
 module.exports = gitwalk = (repo_expr, callback) ->
-    callback repo_select repo_expr
+  callback repo_select repo_expr
