fb84615385d38a6a2617828e532adf7d17c09b9b
Radek Pazdera <me@radek.io>
2015-10-29 17:36:50 +0000

    build: Fixing the build

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var gulp = require('gulp'),
     mocha = require('gulp-mocha'),
     es = require('event-stream'),
     rename = require("gulp-rename"),
-    debug = require('gulp-debug');
+    debug = require('gulp-debug'),
+    insert = require('gulp-insert');
 
 require('coffee-script/register');
 
@@ -14,26 +15,24 @@ var srcDir = './src/',
     testDir = './test/';
 
 gulp.task('lint', function () {
-    return gulp.src([srcDir + '*.coffee'])
+    return gulp.src([srcDir + '**/*.coffee'])
                .pipe(coffeelint())
                .pipe(coffeelint.reporter());
 });
 
 gulp.task('build', function() {
-    src = gulp.src([srcDir + '**/*.coffee'])
-              .pipe(debug())
+    src = gulp.src([srcDir + 'lib/**/*.coffee'])
+              /*.pipe(debug())*/
               .pipe(coffee({bare: true}).on('error', gutil.log))
-              .pipe(gulp.dest(outDir));
+              .pipe(gulp.dest(outDir + "/lib"));
 
-    /*bin = gulp.src([binDir + '*.coffee'])
+    bin = gulp.src([srcDir + 'bin/**/*.coffee'])
+              /*.pipe(debug())*/
               .pipe(coffee({bare: true}).on('error', gutil.log))
-              .pipe(rename(function (path) {
-                  path.extname = "";
-              }))
-              .pipe(gulp.dest(binDir));*/
+              .pipe(insert.prepend("#!/usr/bin/env node\n\n"))
+              .pipe(gulp.dest(outDir + "/bin"));
 
-    return src;
-    //return es.concat(src, bin);
+    return es.concat(src, bin);
 });
 
 gulp.task('test', function() {
